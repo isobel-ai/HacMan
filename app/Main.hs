5748a432ef36e9b6924bf310b4fced9143a6a60e
Isobel Ibironke <isobel_ibironke@icloud.com>
2022-07-13 19:33:59 +0100

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -17,15 +17,11 @@ import Display
 import Maze
 import Sprites
 
-{- The game has five possible states.
-   * State derives Eq as the equality instance for it is obvious.
--}
+-- The game has five possible states.
 data State = StartGame | Play | Won | Lost | Finished
   deriving Eq
 
-{- The ghosts have two possible states.
-   * GhostState derives Eq as the equality instance for it is obvious.
--}
+-- The ghosts have two possible states.
 data GhostState = Normal | Scared
   deriving Eq
 
@@ -51,24 +47,11 @@ data PacManGame = PacManGame { gameMaze :: Maze         -- The Maze the game is 
                              , movePeriod :: Float      -- The value that determines how often the sprites move.
                              }
 
-{- Returns the CoOrds of the ghosts in the game's level.
-   * RecordWildCards are used in this method to make accessing fields more concise.
-   * take is used as it's a safe function - if the level isn't in the range 1-4 an empty list is returned.
-   * This function is defined on the top level as it's used in multiple other functions. 
--}
+-- Returns the CoOrds of the ghosts in the game's level.
 getGhostLocs :: PacManGame -> [CoOrd] 
 getGhostLocs game@PacManGame{..} = take level [inkyLoc,blinkyLoc,pinkyLoc,clydeLoc]
 
-{- Determines the new location of a ghost (called when pacMan is in the same location as a ghost)
-   * RecordWildCards are used in this method to make accessing fields more concise.
-   * Guards are used over if or case statements as there are several choices and there's more than one type of
-     condition.  
-   * A where clause is used for definitions that aren't used anywhere but in this function. pacManIndex and 
-     ghostIndex are generated the same way so a function is declared that can generate both of them, which makes
-     their definitions clearer. fromMaybe is used in this method over fromJust as it's a safer function. Its 
-     default value results in the ghost moving to Clyde's starting position.
-   * This function is defined on the top level as it's used in multiple other functions. 
--}
+-- Determines the new location of a ghost (called when pacMan is in the same location as a ghost)
 newLoc :: PacManGame -> CoOrd -> CoOrd 
 newLoc game@PacManGame{..} coOrd
   | pacManIndex /= ghostIndex = coOrd
@@ -85,9 +68,7 @@ newLoc game@PacManGame{..} coOrd
         ghostIndex :: Int 
         ghostIndex = coOrdIndex coOrd
 
-{- The game's starting state.
-   * randomRs, imported from System.Random, is used to generate an infinite list of random numbers within the
-     given range using the given standard generator. 
+{- The game's starting state. 
    * The chosen movePeriod makes sprites move about three times a second.
    * The moveTimer is set to equal the movePeriod so the sprites' first move occurs as soon as the user starts
      playing the game.
@@ -114,10 +95,7 @@ startGame = PacManGame { gameMaze = startMaze
                        , moveTimer = movePeriod startGame
                        } 
 
-{- Application entry point: allows the user to play the game.
-   * The play function and InWindow data-type are imported from Graphics.Gloss.Interface.Pure.Game in order to run 
-     the game. 
--}
+-- Application entry point: allows the user to play the game.
 main :: IO ()
 main = play 
         (InWindow "Pac-Man" windowSize windowOffset)
@@ -128,20 +106,7 @@ main = play
         handleInput
         update
 
-{- Renders the game by rendering the maze then rendering the sprites on top.
-   * RecordWildCards are used in this method to make accessing fields more concise.
-   * scale, color, text, Blank and pictures are imported from Graphics.Gloss.Data.Picture to create and edit 
-     Pictures.
-   * let-in clauses are used to define functions and defintions that are only used within the function, so 
-     prevents the namespace from being clogged. The clauses are nested to make clear what functions/definitions
-     work together.
-   * The (.) operator is used to compose functions in editTextPic as it's a higher order function that outputs a
-     function.
-   * case statements are used where pattern matching can be used and there are more that two options; if-statements
-     are used where there are two options, as it makes the code flow better.
-   * A lambda function is used in the ghostPictures function to produce a function that outputs a list containing
-     a Blank Picture regardless of input. This method is more consise than defining a full function.
--}
+-- Renders the game by rendering the maze then rendering the sprites on top.
 renderGame :: PacManGame -> Picture
 renderGame game@PacManGame{..} = 
   let editTextPic :: Int -> Int -> (Picture -> Picture) 
@@ -178,16 +143,6 @@ renderGame game@PacManGame{..} =
                                     
 {- Responds to user input: WASD is used to control PacMan's direction and the spacebar to restart the game or move
    to the next level. Any other input is ignored.
-   * Event, Key and SpecialKey are imported from Graphics.Gloss.Interface.Pure.Game in order to allow the user to
-     interact with the game via the keyboard. 
-   * Top-level pattern matching is used for this function as it makes the three distinct cases very clear.
-   * RecordWildCards aren't used in the first version of the function as there are two PacManGame variables so
-     using accessor methods without naming the PacManGame they are refering to would make the function unclear.
-   * let-in and where clauses are used to define functions and definitions that are only used in the handleInput 
-     function, which prevents clogging of the namespace. nested clauses are used to emphasise which functions and
-     definitions correspond with each other.  
-   * case statements are used where there are many options to choose from and the conditions can be pattern 
-     matched, whereas if statements are used when there's two options.
 -}
 handleInput :: Event -> PacManGame -> PacManGame
 handleInput (EventKey (Char c) _ _ _) game =
@@ -221,17 +176,7 @@ handleInput (EventKey (SpecialKey KeySpace) _ _ _) game@PacManGame{..} = startGa
 
 handleInput e game = game
 
-{- Updates the PacManGame record where necessary.
-   * The mod' and elems functions and the (\\) operator are imported from the Data.Fixed, Data.Map and Data.List
-     respectively, in order to make the function more consise (by not having to redefine functions with the same
-     functionality here). 
-   * RecordWildCards are used in this method to make accessing fields more concise.
-   * if statements are used throughout as they follow the logical flow of the selections they represent.
-   * let-in clauses are used to define functions and definitions that are only ever called via the top-level
-     function. Some definitions were made to remove repeated identical function calls (e.g. mazeSquares).
-     Nested let-in clauses are used to emphasise where functions/definitions are used, however many of the lower
-     level functions in the update function link together, so they're mostly left separate for clarity. 
--}
+-- Updates the PacManGame record where necessary.
 update :: Float -> PacManGame -> PacManGame
 update timeElapsed game@PacManGame{..} = 
   if state == Play
@@ -277,20 +222,7 @@ update timeElapsed game@PacManGame{..} =
 
     else game
 
-{- Moves PacMan based on the latest direction set and moves the ghosts randomly.
-   * The (!?) operator is imported from Data.Map to find the Squares associated with input CoOrds in the Maze. It's
-     used alongside the fromMaybe function in Data.Maybe. This is safer than the (!) operator and a more consise 
-     method than defining a new function with the same purpose.
-   * RecordWildCards are used in this method to make accessing fields more concise.
-   * let-in clauses are used to define functions and definitions that are only used in the top-level move function,
-     which prevents clogging of the namespace. Nested let-ins are used where functions/definitons chain together,
-     but only if the formatting of the let-in block doesn't decrease the clarity of the overall function.
-   * case statements are used where there are many options that can be pattern matched and if statements are used
-     where there's two options.
-   * The partial operator (!!) is used in randDir as randGen is an infinite list so any positive number input won't
-     result in an error. Also, the randDir function is protected against negative input, further reducing the chance
-     of the partial operator leading to the game crashing.
--}
+-- Moves PacMan based on the latest direction set and moves the ghosts randomly.
 move :: PacManGame -> PacManGame 
 move game@PacManGame{..} = 
   let nextLocSquare :: CoOrd -> Square
